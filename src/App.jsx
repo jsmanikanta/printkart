a8fc76d77be7d414ea3544ac165ffe42462a5cc5
JEEVANA SANTHOSH MANIKANTA REDDY <jsmanikanta13@gmail.com>
2026-03-04 20:50:26 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import { Routes, Route } from "react-router-dom";
-import React, { useState, useEffect } from "react";
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+
 import Header from "./Header";
+import Footer from "./Footer";
+
 import Cart from "./Cart";
 import VideoHelpSection from "./Help";
 import OrderPrints from "./OrderPrints";
 import Signup from "./Signup";
 import Login from "./Login";
 import ForgotPassword from "./Forgotpass";
-import Footer from "./Footer";
 import Profile from "./Profile";
 import Addlocation from "./Addlocation";
 import PickupAddress from "./Getlocations";
@@ -19,23 +21,25 @@ function App() {
   return (
     <>
       <Header />
-    <Routes>
+      <Routes>
+        <Route path="/" element={<OrderPrints />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/prints-cart" element={<Cart />} />
-        <Route path="/" element={<OrderPrints />} />
         <Route path="/forgotpassword" element={<ForgotPassword />} />
-        <Route path="/profile" element={<Profile/>} />
+        <Route path="/prints-cart" element={<Cart />} />
+        <Route path="/profile" element={<Profile />} />
         <Route path="/help" element={<VideoHelpSection />} />
         <Route path="/mylocations" element={<PickupAddress />} />
         <Route path="/addlocation" element={<Addlocation />} />
         <Route path="/settings" element={<Settings />} />
         <Route path="/student-details" element={<StudentInformation />} />
         <Route path="/faq" element={<FAQ />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
+
       <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
