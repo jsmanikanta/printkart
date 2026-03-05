0fc8b85d1a98dd901903d662026041b5b607004f
JEEVANA SANTHOSH MANIKANTA REDDY <jsmanikanta13@gmail.com>
2026-03-05 19:47:05 +0530

    Update AdminPrints.jsx

--- src/AdminPrints.jsx
+++ src/AdminPrints.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import Loader from "../../frontend/src/Loading";
-import { api_path } from "../../frontend/data";
+import Loader from "./Loading";
+import { api_path } from "../data";
 import "./styles/adminprints.css";
 
 export default function AdminPrints() {
