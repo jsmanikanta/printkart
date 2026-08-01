9b34b2476baaeeff34b46a8b0c437f164a9e633d
J S Manikanta <jsmanikanta13@gmail.com>
2026-08-01 15:03:27 +0530

    azure

--- data.js
+++ data.js
@@ -1,2 +1,2 @@
 //export const api_path = "http://localhost:5000";
-export const api_path = "https://mybookhub-backend-qx9y.onrender.com";
+export const api_path = "https://mybookhub-akfnanayeqc0f2de.centralindia-01.azurewebsites.net";
